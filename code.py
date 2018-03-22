99b220508f5b0af5030eecf1cabc3c6faf67f70f
LucasAntunes <lucas.lukinha67@gmail.com>
2018-03-22 16:28:43 -0300

    Reforms in the main structure of decision

--- code.py
+++ code.py
@@ -3,70 +3,45 @@ from writer import writer
 
 def transcriber():
     Text = input(" Input Text: ")
-    aux = 2;
+    Text = Text.lower()
 
     print()
+    characters = {
+        "a" : 0,
+        "b" : 1,
+        "c" : 2,
+        "d" : 3,
+        "e" : 4,
+        "f" : 5,
+        "g" : 6,
+        "h" : 7,
+        "i" : 8,
+        "j" : 9,
+        "k" : 10,
+        "l" : 11,
+        "m" : 12,
+        "n" : 13,
+        "o" : 14,
+        "p" : 15,
+        "q" : 16,
+        "r" : 17,
+        "s" : 18,
+        "t" : 19,
+        "u" : 20,
+        "v" : 21,
+        "w" : 22,
+        "x" : 23,
+        "y" : 24,
+        "z" : 25,
+        " " : 26
+    }
+
     #controls the height
     for i in range(6):
         print(" ", end="")
         #controls the selected element
         for j in range(len(Text)):
-
-            if((Text[j] == "a") or (Text[j] == "A")):
-                aux = 0
-            elif ((Text[j] == "b") or (Text[j] == "B")):
-                aux = 1
-            elif ((Text[j] == "c") or (Text[j] == "C")):
-                aux = 2
-            elif ((Text[j] == "d") or (Text[j] == "D")):
-                aux = 3
-            elif ((Text[j] == "e") or (Text[j] == "E")):
-                aux = 4
-            elif ((Text[j] == "f") or (Text[j] == "F")):
-                aux = 5
-            elif ((Text[j] == "g") or (Text[j] == "G")):
-                aux = 6
-            elif ((Text[j] == "h") or (Text[j] == "H")):
-                aux = 7
-            elif ((Text[j] == "i") or (Text[j] == "I")):
-                aux = 8
-            elif ((Text[j] == "j") or (Text[j] == "J")):
-                aux = 9
-            elif ((Text[j] == "k") or (Text[j] == "K")):
-                aux = 10
-            elif ((Text[j] == "l") or (Text[j] == "L")):
-                aux = 11
-            elif ((Text[j] == "m") or (Text[j] == "M")):
-                aux = 12
-            elif ((Text[j] == "n") or (Text[j] == "N")):
-                aux = 13
-            elif ((Text[j] == "o") or (Text[j] == "O")):
-                aux = 14
-            elif ((Text[j] == "p") or (Text[j] == "P")):
-                aux = 15
-            elif ((Text[j] == "q") or (Text[j] == "Q")):
-                aux = 16
-            elif ((Text[j] == "r") or (Text[j] == "R")):
-                aux = 17
-            elif ((Text[j] == "s") or (Text[j] == "S")):
-                aux = 18
-            elif ((Text[j] == "t") or (Text[j] == "T")):
-                aux = 19
-            elif ((Text[j] == "u") or (Text[j] == "U")):
-                aux = 20
-            elif ((Text[j] == "v") or (Text[j] == "V")):
-                aux = 21
-            elif ((Text[j] == "w") or (Text[j] == "W")):
-                aux = 22
-            elif ((Text[j] == "x") or (Text[j] == "X")):
-                aux = 23
-            elif ((Text[j] == "y") or (Text[j] == "Y")):
-                aux = 24
-            elif ((Text[j] == "z") or (Text[j] == "Z")):
-                aux = 25
-            else:
-                aux = 26
-            writer(aux, i)
+            writer(characters[Text[j]], i)
         print()
 
 def main():
