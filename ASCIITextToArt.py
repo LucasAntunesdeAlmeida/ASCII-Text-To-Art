ea164ed909f0ccf09349e8e38087bdc7871a9f9d
Lucas Antunes de Almeida <lucas.lukinha67@gmail.com>
2019-06-04 17:39:15 -0300

    add colors

--- ASCIITextToArt.py
+++ ASCIITextToArt.py
@@ -3,6 +3,29 @@ class ASCIITextToArt:
         self.settings = {}
         self.font = {}
         self.setFont("_fonts/default.txt")
+        self.setStyles()
+
+    def setStyles(self):
+        self.styles = {
+            "white":"\033[97m",
+            "black":"\033[30m",
+            "red":"\033[31m",
+            "lightRed":"\033[91m",
+            "green":"\033[32m",
+            "lightGreen":"\033[92m",
+            "yellow":"\033[33m",
+            "lightYellow":"\033[93m",
+            "blue":"\033[34m",
+            "lightBlue":"\033[94m",
+            "magenta":"\033[35m",
+            "lightMagenta":"\033[95m",
+            "cyan":"\033[36m",
+            "lightCyan":"\033[96m",
+            "gray":"\033[90m",
+            "lightGray":"\033[37m",
+            "end":"\033[0m",
+        }
+        
 
     def setFont(self, file):
         ref_archive = open(file, "r")
@@ -44,7 +67,17 @@ class ASCIITextToArt:
             for j in range(self.settings['height']):
                 print(self.font[i][j])
 
-    def getText(self, text):
+    def getStyle(self, style):
+        style = style.split()
+        appearance = ''
+
+        for element in style:
+            if element in self.styles:
+                appearance += self.styles[element]
+
+        return appearance
+
+    def getText(self, text, style=''):
         art = ''
 
         if(self.settings['lowercase'] == 0):
@@ -55,21 +88,23 @@ class ASCIITextToArt:
                 art += self.font[letter][i]
             art += '\n'
         
+        art = self.getStyle(style) + art + self.getStyle('end')
+
         return art
 
-    def getList(self, text):
-        return self.getText(text).split('\n')
+    def getList(self, text, style=''):
+        return self.getText(text, style).split('\n')
 
-    def getMatrix(self, text):
-        art = [[i] for i in self.getList(text)]
+    def getMatrix(self, text, style=''):
+        art = [[i] for i in self.getList(text, style)]
         art.pop()
         return art 
 
-    def printText(self, text):
-        print(self.getText(text))
+    def printText(self, text, style=''):
+        print(self.getText(text, style))
 
 if __name__ == "__main__":
     art = ASCIITextToArt()
-    art.printText("ASCII Text")
-    art.printText("To Art")
+    art.printText("ASCII Text", style='bold lightMagenta')
+    art.printText("To Art", style='lightMagenta')
     print("by:Lucas Antunes de Almeida")
